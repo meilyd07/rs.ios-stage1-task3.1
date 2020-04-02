dc46feffe6cc1ef0f33a00eae709963bc6cf31b4
MeilyD07 <meilyd07@gmail.com>
2020-04-03 01:31:47 +0300

    feat: add polynomial function

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,31 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+
+    NSInteger countItems = [numbers count];
+    if (countItems > 0 ) {
+        NSMutableString *resultString = [NSMutableString new];
+        for (int i = 0; i < countItems; i++) {
+            
+            int currentInt = [((NSNumber *)[numbers objectAtIndex:i]) intValue];
+            int absCurrentInt = abs(currentInt);
+            NSInteger currentDegree = countItems - i - 1;
+            
+            if (currentInt != 0) {
+                [resultString appendFormat:@"%s%@%@%@",
+                 (currentInt > 0) ? ((currentDegree == countItems - 1) ? "" : " + ") : ((currentDegree == countItems - 1) ? "-" : " - "),
+                 ((currentDegree == 0) ||
+                  ((currentDegree > 0) && (absCurrentInt != 1))
+                  ) ? [NSString stringWithFormat:@"%d", absCurrentInt] : @"",
+                 (currentDegree > 0) ? @"x" : @"",
+                 (currentDegree > 1) ? [NSString stringWithFormat:@"^%ld", (long)currentDegree] : @""
+                 ];
+            }
+        }
+        
+        return resultString;
+    } else {
+        return nil;
+    }
 }
 @end
