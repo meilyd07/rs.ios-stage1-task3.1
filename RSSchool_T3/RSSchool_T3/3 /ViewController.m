d65abe179f202eb592c40385118e95c6ab9b454d
MeilyD07 <meilyd07@gmail.com>
2020-04-04 02:43:42 +0300

    feat: change position and add optimizations

--- RSSchool_T3/RSSchool_T3/3 /ViewController.m
+++ RSSchool_T3/RSSchool_T3/3 /ViewController.m
@@ -1,6 +1,6 @@
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
 @property (strong, nonatomic) UIView *viewResultColor;
 @property (strong, nonatomic) UITextField *textFieldRed;
 @property (strong, nonatomic) UITextField *textFieldGreen;
@@ -41,57 +41,54 @@ static int const kLargeRowDistance = 50;
 - (void)setupSubviews {
     
     self.labelResultColor = [UILabel new];
-    self.labelResultColor.accessibilityIdentifier = @"labelResultColor";
-    [self.labelResultColor setText:@"Color"];
-    [self.view addSubview:self.labelResultColor];
-
+    [self setupLabel:self.labelResultColor Named:@"labelResultColor" Labeled:@"Color"];
     self.viewResultColor = [[UIView alloc] initWithFrame:CGRectZero];
     self.viewResultColor.accessibilityIdentifier = @"viewResultColor";
     self.viewResultColor.backgroundColor = UIColor.darkGrayColor;
     [self.view addSubview:self.viewResultColor];
     
     self.labelRed = [UILabel new];
-    self.labelRed.accessibilityIdentifier = @"labelRed";
-    [self.labelRed setText:@"RED"];
-    [self.view addSubview:self.labelRed];
-    
+    [self setupLabel:self.labelRed Named:@"labelRed" Labeled:@"RED"];
     self.textFieldRed = [[UITextField alloc] init];
-    self.textFieldRed.accessibilityIdentifier = @"textFieldRed";
-    self.textFieldRed.borderStyle = UITextBorderStyleRoundedRect;
-    self.textFieldRed.placeholder = @"0..255";
-    [self.view addSubview:self.textFieldRed];
+    [self setupTextField: self.textFieldRed Named:@"textFieldRed"];
     
     self.labelGreen = [UILabel new];
-    self.labelGreen.accessibilityIdentifier = @"labelGreen";
-    [self.labelGreen setText:@"GREEN"];
-    [self.view addSubview:self.labelGreen];
-    
+    [self setupLabel:self.labelGreen Named:@"labelGreen" Labeled:@"GREEN"];
     self.textFieldGreen = [[UITextField alloc] init];
-    self.textFieldGreen.accessibilityIdentifier = @"textFieldGreen";
-    self.textFieldGreen.borderStyle = UITextBorderStyleRoundedRect;
-    self.textFieldGreen.placeholder = @"0..255";
-    [self.view addSubview:self.textFieldGreen];
+    [self setupTextField:self.textFieldGreen Named:@"textFieldGreen"];
     
     self.labelBlue = [UILabel new];
-    self.labelBlue.accessibilityIdentifier = @"labelBlue";
-    [self.labelBlue setText:@"BLUE"];
-    [self.view addSubview:self.labelBlue];
-    
+    [self setupLabel:self.labelBlue Named:@"labelBlue" Labeled:@"BLUE"];
     self.textFieldBlue = [[UITextField alloc] init];
-    self.textFieldBlue.accessibilityIdentifier = @"textFieldBlue";
-    self.textFieldBlue.borderStyle = UITextBorderStyleRoundedRect;
-    self.textFieldBlue.placeholder = @"0..255";
-    [self.view addSubview:self.textFieldBlue];
+    [self setupTextField:self.textFieldBlue Named:@"textFieldBlue"];
     
     self.buttonProcess = [UIButton new];
     self.buttonProcess.accessibilityIdentifier = @"buttonProcess";
     self.buttonProcess.backgroundColor = [UIColor clearColor];
-    [self.buttonProcess setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [self.buttonProcess setTitleColor:[UIColor systemBlueColor] forState:UIControlStateNormal];
     [self.buttonProcess setTitle:@"Process" forState:UIControlStateNormal];
-
     [self.buttonProcess sizeToFit];
+    [self.buttonProcess addTarget:self action:@selector(buttonProcessDidTap:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:self.buttonProcess];
     
+}
+
+- (void)setupLabel:(UILabel *)label Named:(NSString *)identifier Labeled:(NSString *)text {
+    label.accessibilityIdentifier = identifier;
+    [label setText:text];
+    [self.view addSubview:label];
+}
+
+- (void)setupTextField:(UITextField *)textField Named:(NSString *)identifier{
+    textField.accessibilityIdentifier = identifier;
+    textField.delegate = self;
+    textField.placeholder = @"0..255";
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    [self.view addSubview:textField];
+}
+
+- (void)buttonProcessDidTap:(UIButton *)button {
+    NSLog(@"Process...");
 }
 
 - (void)setupConstraints {
@@ -101,7 +98,7 @@ static int const kLargeRowDistance = 50;
     [self setConstraintsFor:self.labelRed Sized:kLabelWidth And:self.textFieldRed From:self.viewResultColor.bottomAnchor With:kLineDistance];
     [self setConstraintsFor:self.labelGreen Sized:kLabelWidth And:self.textFieldGreen From:self.textFieldRed.bottomAnchor With:kLineDistance];
     [self setConstraintsFor:self.labelBlue Sized:kLabelWidth And:self.textFieldBlue From:self.textFieldGreen.bottomAnchor With:kLineDistance];
-    [self setButtonCpnstraints];
+    [self setButtonConstraints];
 }
 
 - (void)setConstraintsFor:(UILabel *)label Sized:(CGFloat)width And:(UIView *)view From:(NSLayoutAnchor *)anchor With:(CGFloat)distance{
@@ -112,15 +109,17 @@ static int const kLargeRowDistance = 50;
     [view.topAnchor constraintEqualToAnchor:anchor constant:distance].active = YES;
     [self.view.trailingAnchor constraintEqualToAnchor:view.trailingAnchor constant:kRightBorderWidth].active = YES;
     [label.centerYAnchor constraintEqualToAnchor:view.centerYAnchor constant:0].active = YES;
+    [label.heightAnchor constraintEqualToAnchor:view.heightAnchor multiplier:1].active = YES;
     [label.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:kLeftBorderWidth].active = YES;
     [label.widthAnchor constraintEqualToConstant:width].active = YES;
     [label.trailingAnchor constraintEqualToAnchor:view.leadingAnchor constant:kRowDistance].active = YES;
 }
 
-- (void)setButtonCpnstraints {
+- (void)setButtonConstraints {
     self.buttonProcess.translatesAutoresizingMaskIntoConstraints = NO;
     [self.buttonProcess.topAnchor constraintEqualToAnchor:self.textFieldBlue.bottomAnchor constant:kLargeRowDistance].active = YES;
     [self.buttonProcess.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor constant:0].active = YES;
 }
 
 @end
+
